d63010de6e4ab228ec05ebbfc40d8c29455af5df
andrewhansbury <73508007+andrewhansbury@users.noreply.github.com>
2021-05-01 02:59:56 -0400

    identifying trades from messages

--- discordwebscraper.py
+++ discordwebscraper.py
@@ -1,7 +1,11 @@
+import string
+from typing import List
 from selenium import webdriver
 import discord
 import time
 import codecs
+import os
+os.system('cls')
 
 DRIVER_PATH = 'chromedriver.exe'
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
@@ -22,31 +26,52 @@ def login():
         "//*[@id=\"app-mount\"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/button[2]")
 
     submit_button.click()
+    print("Logged In! \n")
 
 
 # change this to a more accurate name tho tbh
-def getTrade():
+def importTrades():
     messages = []
     driver.implicitly_wait(10)
     sent = driver.find_elements_by_class_name(
         "message-2qnXI6")
     with codecs.open('Messages.txt', 'w', "utf-8-sig") as file:
         for message in sent:
+            messages.append((message.text.lower()))
             file.write(message.text)
 
     file.close()
-    # with open('Messages.txt', 'w') as file:
-    #     for message in messages:
-    #         file.write(message)
-    # file.close()
+
+    return messages
+
+
+def getTrade(messages):
+    trades = []
+    #assert isinstance(trades, dict)
+    assert isinstance(messages, List)
+    # for message in messages:
+    #     assert isinstance(message, str)
+    #     if "coin:" in message:
+    #         message = message[message.index("coin"):]
+    #     else:
+    #         messages.pop(message)
+    # for message in messages:
+
+    trade_list = [x for x in messages if "coin:" in x]
+    for message in trade_list:
+        message = message[message.index("coin"):]
+        print(message)
+        print()
 
 
 def main():
 
     driver.get(
         'https://discord.com/login?redirect_to=%2Fchannels%2F646144563802800139%2F834441984314834944')
+    print("Connected to Discord! \n")
     login()
-    getTrade()
+    trade_array = importTrades()
+    getTrade(trade_array)
 
 
 if __name__ == "__main__":

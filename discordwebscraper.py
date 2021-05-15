3fd99caf22453a49813517cbb3bf05cec55ed770
andrewhansbury <73508007+andrewhansbury@users.noreply.github.com>
2021-05-15 01:15:31 -0400

    added main

--- discordwebscraper.py
+++ discordwebscraper.py
@@ -11,31 +11,48 @@ os.system('cls')
 
 
 DRIVER_PATH = 'chromedriver.exe'
-#driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
 
 def connect():
     driver.get(
         'https://discord.com/login?redirect_to=%2Fchannels%2F646144563802800139%2F834441984314834944')
-    print("Connected to Discord! \n")
+    print("Connected to Discord! \n\n")
 
 
 def login():
+    try:
+        email_input = driver.find_element_by_xpath(
+            "/html/body/div/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[2]/div[1]/div/div[2]/input")
+        email_input.send_keys("andrew.hansbury@gmail.com")
 
-    email_input = driver.find_element_by_xpath(
-        "//*[@id=\"app-mount\"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/div[1]/div/div[2]/input")
-    email_input.send_keys("andrew.hansbury@gmail.com")
+        pass_input = driver.find_element_by_xpath(
+            "//*[@id=\"app-mount\"]/div[2]/div/div/form/div/div/div[1]/div[2]/div[2]/div/input")
 
-    pass_input = driver.find_element_by_xpath(
-        "//*[@id=\"app-mount\"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/div[2]/div/input")
+        pass_input.send_keys("Andr1234")
 
-    pass_input.send_keys("Andr1234")
+        submit_button = driver.find_element_by_xpath(
+            "//*[@id=\"app-mount\"]/div[2]/div/div/form/div/div/div[1]/div[2]/div[2]/div/input")
 
-    submit_button = driver.find_element_by_xpath(
-        "//*[@id=\"app-mount\"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/button[2]")
+        submit_button.click()
+        print("Logged In! \n")
+    except:
+        pass
 
-    submit_button.click()
-    print("Logged In! \n")
+
+def saveToFile():
+    messages = []
+    driver.implicitly_wait(20)
+    print("Logged In! \n\n")
+    sent = driver.find_elements_by_class_name(
+        "message-2qnXI6")
+    for message in sent:
+        messages.append((message.text.lower()))
+
+    with open('Messages.txt', 'wb') as file:
+        pickle.dump(messages, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+    file.close()
 
 
 # change this to a more accurate name tho tbh
@@ -51,19 +68,6 @@ def importTrades():
     file.close()
 
     return messages
-
-
-def saveToFile():
-    messages = []
-    driver.implicitly_wait(10)
-    sent = driver.find_elements_by_class_name(
-        "message-2qnXI6")
-    for message in sent:
-        messages.append((message.text.lower()))
-
-    with open('Messages.txt', 'wb') as file:
-        pickle.dump(messages, file, protocol=pickle.HIGHEST_PROTOCOL)
-    file.close()
 
 
 def getTrade(messages):
@@ -93,6 +97,7 @@ def formatTrade(messages):
     assert isinstance(messages, List)
 
     for message in messages:
+        print(message)
         trade = {}
         assert isinstance(message, str)
 
@@ -117,6 +122,13 @@ def formatTrade(messages):
             str5 = message.split("stop loss:")[1].strip()
             stop_loss = str5[0:str5.index('\n')]
             trade['stop loss'] = stop_loss
+        if "take profits:" in message or "take profit" in message:
+            try:
+                str6 = message.split("take profits:")[1].strip()
+            except:
+                str6 = message.split("take profit:")[1].strip()
+            take_profit = str6[0:str6.index('\n')]
+            trade['take profits'] = take_profit
         saved_trades.append(trade)
 
     with open('saved_trades.txt', 'wb') as file:
@@ -130,14 +142,15 @@ def loadTrades():
     all_trades = []
     with open('saved_trades.txt', 'rb') as file:
         all_trades = pickle.load(file)
-    print(all_trades)
+    for trade in all_trades:
+        print(trade)
     return all_trades
 
 
 def main():
-    # connect()
-    # login()
-    # saveToFile()
+    connect()
+    login()
+    saveToFile()
     with open('Messages.txt', 'rb') as file:
         trade_array = pickle.load(file)
     tradelist = getTrade(trade_array)

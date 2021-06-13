116c9c6def00282e269cf8593d273299c5cc9cac
andrewhansbury <73508007+andrewhansbury@users.noreply.github.com>
2021-06-13 10:15:35 -0400

    can place trades
    (need to fix adjusting leverage)

--- bnbot.py
+++ bnbot.py
@@ -1,5 +1,7 @@
+from typing import List
 from binance.enums import *
 from binance.client import Client
+import time
 
 import config
 
@@ -22,15 +24,29 @@ print(futures['totalWalletBalance'])
 
 # FIND MAX AND MIN LEVRAGE OF COIN
 
-def maxLeverage(pair):
+def maxLeverage(symbol):
+    start_time = time.time()
+    assert isinstance(symbol, str)
+    symbol = symbol.upper()
     result = client.futures_leverage_bracket()
     for x in result:
-        if 'BTCUSDT' in x['symbol']:
-            print(x)
-            print('\n')
+        if symbol == x['symbol']:
+            print("--- %s seconds ---" % (time.time() - start_time))
+            return x['brackets'][0]['initialLeverage']
+    return "Not Found"
 
 
-print(maxLeverage('ETHUSDT'))
+print(maxLeverage('DOGEUSDT'))
+x = {'symbol': 'DOGEUSDT', 'leverage': 45}
+print(client.futures_change_leverage(
+    **x))
+client.futures_coin_change_leverage(**x)
+
+
+#client.futures_change_margin_type(symbol='DOGEUSDT', marginType='CROSSED')
+x = {'symbol': 'DOGEUSDT', 'side': 'BUY', 'type': 'MARKET', 'quantity': 100.0}
+
+client.futures_create_order(**x)
 
 
 def place_trade(trade):
